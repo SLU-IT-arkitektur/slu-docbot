7037b8ba8e3aadac96d6b2fca226991fd39aaa97
hellgrenj <hellgrenj@gmail.com>
2025-01-03 10:00:05 +0100

    improved the skaffold_delete_all script a bit

--- skaffold_delete_all.sh
+++ skaffold_delete_all.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+# ANSII Colors
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 skaffold_files=$(find . -name 'skaffold.*.yaml')
 skaffold_files="$skaffold_files skaffold.yaml"
 echo "this will run the following skaffold delete commands:"
 for file in $skaffold_files
 do
-    echo "skaffold delete -f $file"
+    echo "skaffold delete -f $BLUE$file$RESET"
 done
+current_k8s_env=$(kubectl config current-context)
+echo "you are currently targetting the kubectl context $RED$current_k8s_env$RESET"
 echo 'are you sure you want to continue? (y/n)'
 read answer
 if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
